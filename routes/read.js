dce6f57e54045ae83c45fbc68cc4e502382b85e6
Euro <euro@EurodeMacBook-Pro.local>
2018-02-22 22:26:26 +0800

    修改

--- routes/read.js
+++ routes/read.js
@@ -1,25 +1,36 @@
 
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'gz-cdb-dollcvx2.sql.tencentcdb.com',
-  port     : 63174,
-  user     : 'root',
-  password : 'jike95788',
-  database : 'SaveUser'
-});
-
-connection.connect();
-
-var mysqlData=connection.query('SELECT  * FROM `User`');
-// , function (error, results, fields) {
-//   if (error) html=error;
-//   res.send(html);
+ var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//   host: 'gz-cdb-dollcvx2.sql.tencentcdb.com',
+//   port: 63174,
+//   user: 'root',
+//   password: 'jike95788',
+//   database: 'SaveUser'
 // });
 
-const html =mysqlData.toString();
+// connection.connect();
 
-connection.end();
+// var mysqlData = connection.query('SELECT  * FROM `User`');
+// // , function (error, results, fields) {
+// //   if (error) html=error;
+// //   res.send(html);
+// // });
+
+
+// const html = mysqlData.toString();
+
+const html =
+`
+  <html>
+    <Text>
+    123
+    </Text>
+  </html>
+`
+
+//runconnection.end();
 module.exports = (req, res) => {
-    res.send(html);
+  //console.log("%j", mysqlData)
+  res.send(html);
 };
